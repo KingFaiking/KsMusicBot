16e2e54aebca35b4a6088fcd6ddcd7c1ee540194
KingFaiking <41138080+KingFaiking@users.noreply.github.com>
2018-08-05 00:15:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,46 @@
-const Discord = require("discord.js");
-const axios = require("axios");
-const yt = require("ytdl-core");
-const YouTube = require("simple-youtube-api");
-const fs = require("fs");
-const getYTID = require("get-youtube-id");
-const fetchVideoInfo = require("youtube-info");
-const prefix = botSettings.prefix;
-const ytApiKey = botSettings.ytApiKey;
-const youtube = new YouTube(ytApiKey);
-
-const bot = new Discord.Client({
-	disableEveryone: true
+const Discord = require('discord.js');
+const client = new Discord.Client({disableEveryone: true})
+const Music = require('discord.js-musicbot-addon');
+client.login(process.env.BOT_TOKEN);
+client.on('ready', ()=> {
+    console.log('Ready!')
 })
+ Music.start(client, {
+    prefix: "!-", // البرفكس هنا
+    youtubeKey: 'AIzaSyDIv8O9OwZdoTaghK3_SQmyGcQFzkXMW6E', //هنا ال yt v3 api key
+    global: false, // يكون للسيرفرات ؟ خليه false, يكون queue مشترك خليه true            
+    maxQueueSize: 50, // اقصى حجم لل queue        
+    playCmd: 'play', // امر التشغيل
+    volumeCmd: 'vol',     // امر الصوت
+    thumbnailType: 'high',
+    leaveCmd: 'stop',    // امر المغادرة
+    anyoneCanSkip: true, // تبي الكل يقدر يسوي skip? true > yep, false > nope
+    disableLoop: false, // تبي يكون فيه اعادة؟ , true > yep, false > nope
+    searchCmd: 'search', //امر البحث 
+    requesterName: true, 
+    inlineEmbeds: false,     
+    queueCmd: 'queue', // امر القائمة
+    pauseCmd: 'pause', // امر الايقاف المؤقت
+    resumeCmd: 'resume', // امر الاستمرار
+    skipCmd: 'skip', // امر السكب
+    loopCmd: 'loop', // امر الاعادة
+    enableQueueStat: true,
+  });
 
-client.on("ready", async () => {
-	console.log(`Bot is ready! ${bot.user.username}`);
+  client.on('ready', function(){
+    var ms = 1000 ;
+    var setGame = [`${prefix}h | Music `,` ${prefix}he | Music `,`${prefix}hel | Music `,`${prefix}help | Music `];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://twitch.tv/idk`);
+    }, ms);1000
 
 });
-
-
-
-
-
-client.login(process.env.BOT_TOKEN);
