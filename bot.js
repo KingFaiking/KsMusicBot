f102d7a20b6613c5bb85bd8e6e9173befa6afadf
KingFaiking <41138080+KingFaiking@users.noreply.github.com>
2018-11-01 07:17:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,6 +16,8 @@ const ytdl = require('ytdl-core');
 
 const fs = require('fs');
 
+const gif = require("gif-search");
+
 const client = new Discord.Client({disableEveryone: true});
 
 const prefix = "!";
